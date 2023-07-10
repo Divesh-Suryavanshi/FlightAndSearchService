a978eded8682913daf9cbf0ebc34527a7aa8f3a5
Divesh <diveshsisodiads@gmail.com>
2023-07-11 02:10:02 +0530

    first commit to setup the project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
+  const app = express();
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.listen(PORT, () => {
+    console.log(`server is running at port ${PORT}`);
+  });
+};
+
+setupAndStartServer();
